0e1105a4cdafe64bf9c847db84060171de9f2ed1
Curtis Vogt <curtis.vogt@invenia.ca>
2018-04-13 16:13:15 -0500

    Use scalar for get_login

--- src/ECR.jl
+++ src/ECR.jl
@@ -5,23 +5,28 @@ module ECR
 using AWSSDK
 using Mocking
 
-import AWSSDK.ECR: get_authorization_token
+using AWSSDK.ECR: get_authorization_token
 
 """
-    get_login(registry_ids::AbstractVector=String[]) -> Cmd
+    get_login(registry_ids::Union{AbstractString, Integer}="") -> Cmd
 
 Gets the AWS ECR authorization token and returns the corresponding docker login command.
 """
 get_login
 
-function get_login(registry_ids::AbstractVector{<:AbstractString}=String[])
-    resp = if !isempty(registry_ids)
-        @mock get_authorization_token(registryIds=registry_ids)
+function get_login(registry_id::AbstractString="")
+    # Note: Although `get_authorization_token` can take multiple registry IDs at once it
+    # will only return a "proxyEndpoint" for the first registry. Additionally, the
+    # `aws ecr get-login` command returns a `docker login` command for each registry ID
+    # passed in. Because of these factors we'll do our processing on a single registry.
+
+    response = if !isempty(registry_id)
+        @mock get_authorization_token(registryIds=[registry_id])
     else
         @mock get_authorization_token()
     end
 
-    authorization_data = first(resp["authorizationData"])
+    authorization_data = first(response["authorizationData"])
     token = String(base64decode(authorization_data["authorizationToken"]))
     username, password = split(token, ':')
     endpoint = authorization_data["proxyEndpoint"]
@@ -29,6 +34,9 @@ function get_login(registry_ids::AbstractVector{<:AbstractString}=String[])
     return `docker login -u $username -p $password $endpoint`
 end
 
-get_login(registry_ids::AbstractVector{<:Integer}) = get_login(lpad.(registry_ids, 12, '0'))
+get_login(registry_id::Integer) = get_login(lpad(registry_id, 12, '0'))
+
+# AWSTools 0.1.0 deprecation
+@deprecate get_login(ids::AbstractVector) get_login.(ids)
 
 end  # ECR
